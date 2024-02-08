8ede37aca368c8b349ab4a8ee40235bc80c11396
Jhon1505e <108197460+Jhon1505e@users.noreply.github.com>
2024-02-08 18:53:52 -0500

    index

---
+++ components/Footer.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="bg-gray-600 pt-8 relative bottom-0  ">
+    <div class="flex justify-center w-1/2 mx-auto">
+      <div>
+        <img src="/img/logo_zeramic.webp" class="w-60" alt="" />
+      </div>
+      <hr class="rotate-90 w-40 mt-24" />
+      <div class="mt-10">
+        <h2 class="text-white text-4xl ml-3 font-bold">Contacto</h2>
+        <div class="flex gap-2 mt-3">
+          <div class="duration-100 ease-in bg-white hover:opacity-100 opacity-80  border rounded-full w-10 p-1">
+            <a href="https://wa.me/+573002285505" target="_blank" class="flex">
+              <IconsWhatsapp class="w-9 fill-gray-600" />
+            </a>
+          </div>
+
+          <div class="duration-100 bg-white ease-in hover:opacity-100 opacity-80 border rounded-full w-10 p-1">
+            <a
+              href="https://www.facebook.com/zeramic.guatape?_rdc=1&_rdr"
+              target="_blank"
+              class="flex"
+            >
+              <IconsFacebook class="w-5 mt-1 ml-1 fill-gray-600" />
+            </a>
+          </div>
+
+          <div class="duration-100 bg-white ease-in hover:opacity-100 opacity-80 border rounded-full w-10 p-1">
+            <a
+              href="https://www.instagram.com/zeramic.guatape/"
+              target="_blank"
+              class="flex"
+            >
+              <IconsInstagram class="  fill-gray-600" />
+            </a>
+          </div>
+
+          <div class="duration-100 bg-white ease-in hover:opacity-100 opacity-80 border rounded-full w-10 p-1">
+            <a
+              href="https://www.facebook.com/zeramic.guatape?_rdc=1&_rdr"
+              target="_blank"
+              class="flex"
+            >
+              <IconsGmail class="w-6 ml-0.5 mt-1 fill-gray-600" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr class="w-1/2 mx-auto" />
+    <h2 class="text-white text-center py-4 font-medium">
+      Powered by: TailorsDev
+    </h2>
+  </div>
+</template>
