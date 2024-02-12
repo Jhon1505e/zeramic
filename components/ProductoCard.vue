4a1064a767901daeac864c3906890ff09ebeb3cf
David Roman <roman.david@gmail.com>
2024-02-11 22:04:18 -0500

    add composable products

---
+++ components/ProductoCard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  uuid: string
+  content: any
+  slug: string
+}>()
+</script>
+
+<template>
+    <article class="rounded-lg shadow-md p-2" :key="uuid">
+        <NuxtLink :to="`/productos/${slug}`">
+            <h2>{{ content.Nombre }}</h2>
+            <img :src="content.Imagen?.filename" alt="">
+        </NuxtLink>
+    </article>
+</template>
---
+++ pages/productos/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+    const { products, fetchProducts } = useProducts()
+
+    await fetchProducts()
+</script>
+<template>
+    <div>
+        <h1>Productos</h1>
+        <hr>
+        <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-12">
+            <ProductoCard
+                v-for="{content, uuid, slug} in products"
+                :uuid="uuid"
+                :content="content"
+                :slug="slug"
+            />
+        </div>
+        <pre class="text-xs">{{ products }}</pre>
+    </div>
+</template>
