fb14dd4d077ef681e9444a0a00d9514407f49084
Jhon1505e <108197460+Jhon1505e@users.noreply.github.com>
2024-02-20 17:16:10 -0500

    footer nosotros favicon

---
+++ components/BgIndex.vue
@@ -0,0 +1,20 @@
+<template>
+    
+
+        <div
+        class="relative bg-fixed bg-cover bg-center h-full"
+        style="background-image: url('/img/background.png')"
+        >
+        <div class="absolute inset-0 backdrop-brightness-50"></div>
+        <div class="w-full flex justify-center text-center items-center absolute inset-0">
+            <div>
+                <img src="/img/text_index.webp" class="w-2/3 mx-auto" alt="" />
+                <h2 class="text-white text-md mt-8 py-3 px-5 font-bold">
+                    NOS INSPIRA A CREAR BELLAS PIEZAS QUE GUARDAN SU ESENCIA, SUS
+                    FORMAS, SUS COLORES...
+                </h2>
+            </div>
+        </div>
+    
+    </div>
+</template>
