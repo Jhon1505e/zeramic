353074d325a15b010b3045aff24155a19d3de96a
David Roman <roman.david@gmail.com>
2024-03-07 19:19:49 -0500

    add modal Cart

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -40,13 +40,13 @@
         </li>
       </ul>
       <div class="w-1/2 md:w-1/3 flex justify-end pt-1 md:pt-2 pr-10">
-        <NuxtLink
-          to="/compras"
+        <button
+        @click="open = true"
           class="flex rounded-full border border-white hover:scale-105 duration-100 ease-in text-white bg-white/20 px-4 md:px-6 py-1 md:pt-2 gap-2 md:h-10"
         >
           <span class="hidden md:block">Carrito {{ cart.size }}</span>
           <IconsBag class="w-8 md:w-6 stroke-white" />
-        </NuxtLink>
+        </button>
       </div>
       <div class="w-auto text-end pr-10 pt-1 lg:hidden">
         <button class="" @click="modal = true">
@@ -107,5 +107,5 @@
 </template>
 <script setup lang="ts">
 const modal = ref(false);
-const { cart } = useShopping();
+const { open, cart } = useShopping();
 </script>
--- composables/useShopping.ts
+++ composables/useShopping.ts
@@ -1,9 +1,11 @@
 export function useShopping() {
 
     const cart = useState('cart', () => new Map())
+    const open = useState('open', () => false)
 
     function addProduct({ uuid, ...product }: any) {
         cart.value.set(uuid, product)
+        open.value = true
     }
     
     function delProduct(uuid: string) {
@@ -11,6 +13,7 @@ export function useShopping() {
     }
 
     return {
+        open,
         cart,
         addProduct,
         delProduct
