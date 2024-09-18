963379438056f4a67a3473c2db1c55f6f51a673d
Jhon1505e <tailorsdevcom@gmail.com>
2024-09-18 14:30:15 -0500

    Clientes

---
+++ components/ModalClients.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const emit = defineEmits(["close"]);
+</script>
+
+<template>
+  <div class="bg-black/60 h-screen fixed top-0 w-full z-20">
+    <div class="flex justify-center items-center h-full">
+      <div class="bg-white rounded-lg w-1/3 h-1/3">
+        <div class="flex justify-between px-4">
+
+            <h2 class="text-center text-PRP">Información del cliente</h2>
+            <button @click="emit('close')"><IconsClose  class="w-5 stroke-PRP" /></button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
