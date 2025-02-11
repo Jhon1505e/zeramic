bdc281b456b44991ae33c977a15b690a9ebcaf4e
David Román <roman.david@gmail.com>
2025-02-11 03:06:38 -0500

    Actualizar useAuth.ts

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -17,5 +17,18 @@ export const useAuth = () => {
 
     const logout = async () => { }
 
-    return { authUser, login, logout }
+const reset = async (user: any) => { 
+
+
+
+
+    const data = await $fetch('/api/auth/reset', {
+      method: 'POST',
+      body: JSON.stringify(user),
+    });
+    console.log(data);
+    return data;
+}
+
+    return { authUser, login, logout, reset}
 }
