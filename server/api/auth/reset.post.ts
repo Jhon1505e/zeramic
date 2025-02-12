2df39bd90a3ddbd861e0c3ba7f8f61900f6a159f
tailorsdev <roman.david@gmail.com>
2025-02-11 22:43:26 -0500

    add reset pass

--- server/api/auth/reset.post.ts
+++ server/api/auth/reset.post.ts
@@ -1,4 +1,29 @@
 export default defineEventHandler(async (event) => {
     const body = await readBody(event)
-    return body
+
+    const { findClient, updateClient } = fetchClient()
+    const user = await findClient(body.email)
+
+    if (!user) {
+        return { message: 'Usuario no registrado' }
+    }
+    const { sendEmail } = useEmail()
+    const newPass = generatePassword()
+    const password = await hashPassword(newPass)
+
+    const { password: _, ...userData } = user
+
+    const data = await updateClient({
+        email: body.email,
+        password,
+        state: 'active',
+        ...userData
+    })
+
+    const dataEmail = await sendEmail({
+        email: body.email,
+        subject: 'Contraseña actualizada',
+        html: `Tu nueva contraseña es: ${newPass}`
+    })
+    return { data, dataEmail }
 })
--- types/clients.ts
+++ types/clients.ts
@@ -2,9 +2,10 @@ import type { Condition, ObjectId } from "mongodb";
 
 export interface IClient {
     _id?: Condition<ObjectId>;
-    name: string;
+    name?: string;
     email: string;
-    picture: string;
+    password: string;
+    picture?: string;
     phone?: string;
     docType?: string;
     docId?: string;
