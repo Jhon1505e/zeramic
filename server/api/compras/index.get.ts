2b60a195dab8a0c88ce16663c2297a604e28e0f5
tailorsdev <roman.david@gmail.com>
2025-03-06 01:06:49 -0500

    again

--- server/api/compras/index.get.ts
+++ server/api/compras/index.get.ts
@@ -1,5 +1,13 @@
 export default defineEventHandler(async (event) => {
 
+    const clientIP = getRequestHeaders(event)['x-forwarded-for'] || event.node.req.socket.remoteAddress;
+    console.log('clientIP', clientIP)
+
+    // Permitir solo IPs locales o de un proxy confiable
+    if (!clientIP?.startsWith('192.168.') && clientIP !== '127.0.0.1' && clientIP !== '::1') {
+        throw createError({ statusCode: 403, statusMessage: 'Acceso no permitido' });
+    }
+
     const query = getQuery(event)
     const { getCompras } = dataCompras()
     const data = await getCompras(query)
--- server/middleware/user.ts
+++ server/middleware/user.ts
@@ -3,18 +3,6 @@ import { getUserToken } from "../utils/session"
 export default defineEventHandler(async (event) => {
     // ...
     const user = await getUserToken(event)
-    console.log(user);
-
-    const requestHeaders = getRequestHeaders(event);
-
-    const headers = getHeaders(event)
-
-
-    console.log('requestHeaders => ', requestHeaders);
-    
-    console.log('- - - - - - -- - - - - --- - - -');
-    
-    console.log('headers => ', headers);
 
     if (!user) {
         event.context.user = null
