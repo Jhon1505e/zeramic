aadfcfd50104b45c78adfdcfe44a3e2db5b2f4d7
David Román <roman.david@gmail.com>
2025-03-11 05:39:43 -0500

    Update realtime.vue

--- pages/realtime.vue
+++ pages/realtime.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import { RealtimeClient } from '@supabase/realtime-js'
 
-
+const client = new RealtimeClient(REALTIME_URL, {
+  params: {
+    apikey: API_KEY
+  },
+})
     
 </script>
 
