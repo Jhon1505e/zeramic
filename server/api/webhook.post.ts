3eae2e5ebc512dd15b31021fb33b71e4717ec6d6
tailorsdev <roman.david@gmail.com>
2025-03-11 01:28:26 -0500

    Update webhook.post.ts

--- server/api/webhook.post.ts
+++ server/api/webhook.post.ts
@@ -28,8 +28,8 @@ export default defineEventHandler(async (event) => {
     }
     const message = messages.find(() => true);
 
-    const data = await saveMessage(message);
-    console.log('body', data);
+    // const data = await saveMessage(message);
+    console.log('message', message);
 
     return { statusCode: 200, body: { message: 'ok' } }
 })
