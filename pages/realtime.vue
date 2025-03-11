4e3ee252fe36e30857071e781c0c918e65da3efa
David Román <roman.david@gmail.com>
2025-03-11 06:00:17 -0500

    Update realtime.vue

--- pages/realtime.vue
+++ pages/realtime.vue
@@ -6,7 +6,37 @@ const client = new RealtimeClient('https://lzlomcfdumwvwjtbnzcm.supabase.co', {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6bG9tY2ZkdW13dndqdGJuemNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc4NDcyNTIsImV4cCI6MjAwMzQyMzI1Mn0.GZJYjOQ_b5-74mUPyVqaqovHMbucPiAiQ9ouV13A4go"
   },
 })
-    
+
+const channel = client.channel(
+  'presence-test',
+  {
+    config: {
+      presence: {
+        key: ''
+      }
+    }
+  }
+)
+
+channel.on('presence', { event: 'sync' }, () => {
+  console.log('Online users: ', channel.presenceState())
+})
+
+channel.on('presence', { event: 'join' }, ({ newPresences }) => {
+  console.log('New users have joined: ', newPresences)
+})
+
+channel.on('presence', { event: 'leave' }, ({ leftPresences }) => {
+  console.log('Users have left: ', leftPresences)
+})
+
+channel.subscribe(async (status) => {
+  if (status === 'SUBSCRIBED') {
+    const status = await channel.track({ 'user_id': 1 })
+    console.log(status)
+  }
+})
+  
 </script>
 
 <template>
