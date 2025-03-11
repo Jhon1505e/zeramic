4faa20e0556f70bc7068de191f2dc164099bf84a
David Román <roman.david@gmail.com>
2025-03-11 05:42:22 -0500

    Update realtime.vue

--- pages/realtime.vue
+++ pages/realtime.vue
@@ -3,7 +3,7 @@ import { RealtimeClient } from '@supabase/realtime-js'
 
 const client = new RealtimeClient(REALTIME_URL, {
   params: {
-    apikey: API_KEY
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6bG9tY2ZkdW13dndqdGJuemNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc4NDcyNTIsImV4cCI6MjAwMzQyMzI1Mn0.GZJYjOQ_b5-74mUPyVqaqovHMbucPiAiQ9ouV13A4go"
   },
 })
     
